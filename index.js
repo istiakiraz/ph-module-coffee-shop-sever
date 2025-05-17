3dbac5ddad7d0b53e92298105f9a8d87b8d8fac3
istiakiraz <istiakiraz@gmail.com>
2025-05-18 04:00:43 +0600

    added a update method

--- index.js
+++ index.js
@@ -45,6 +45,18 @@ async function run() {
       res.send(result);
     })
 
+    app.post('/coffees/:id', async(req, res)=>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)};
+      const options = { upsert: true };
+      const updateCoffee = req.body;
+      const updateDoc = {
+        $set: updateCoffee
+      }
+      const result = await coffeeCollection.updateOne(filter, updateDoc, options)
+      res.send(result)
+    })
+
     app.delete('/coffees/:id', async(req, res)=>{
       const id = req.params.id;
       const query = {_id : new ObjectId(id)};
